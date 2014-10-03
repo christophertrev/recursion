cb56a6722476dc648b6e24186f5dc1158bc32dd5
Christopher Treviño <christophertrev@gmail.com>
2014-10-03 16:14:59 -0700

    Added sudeo code

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,4 +6,8 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
   // your code here
+  //grab document
+  //index through level to find any elements that have that class
+  //if has any more children index through those
+  //need to look through all elements in the document
 };
