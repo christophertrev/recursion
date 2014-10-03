657001b6029016738f8fabeb32b24d9efa0b491f
Christopher Treviño <christophertrev@gmail.com>
2014-10-03 12:52:12 -0700

    Added funationality for only obj not arrays or literals

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,4 +4,15 @@
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
   // your code goes here
+  var isArray = Array.isArray(obj);
+
+  var str = (isArray? '{' : '[');
+  var keys = Object.keys(obj);
+  for (var i =0; i< keys.length;i++){
+  	str += '"' + key[i] +'":"'+ obj[key]+'"';
+  }
+
+  str += (isArray ? '}' : ']');
+  return str
+
 };
