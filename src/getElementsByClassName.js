13b996f1b7147598296f9cb9c710d7a26c9ac561
Christopher Treviño <christophertrev@gmail.com>
2014-10-03 16:43:50 -0700

    Added a new input to specify a starting element

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,11 +4,14 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className){
+var getElementsByClassName = function(className, startElement){
   // your code here
   //grab document
-  $body = document.body;
-  console.log($body);
+  $startElement = ( startElement ? startElement : document.body)
+  //$body = document.body;
+  console.log($startElement);
+
+
   //index through level to find any elements that have that class
   //if has any more children index through those
   //need to look through all elements in the document
